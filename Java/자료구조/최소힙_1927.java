7223b36afc230ab33e1fa0008bfb5fab68437693
HeeSeung Shin <tlsgmltmd726529@gmail.com>
2021-08-12 17:16:08 +0900

    21.08.12

---
+++ Java/자료구조/최소힙_1927.java
@@ -0,0 +1,37 @@
+package √÷º“»¸_1927;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.PriorityQueue;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		PriorityQueue<Integer> q = new PriorityQueue<Integer>();
+		
+		for(int i=0;i<n;i++) {
+			int x = Integer.parseInt(br.readLine());
+			if(x == 0) {
+				if(q.isEmpty())
+					bw.write(0 + "\n");
+				else
+					bw.write(q.poll() + "\n");
+			}
+			else {
+				q.add(x);
+			}
+		}
+		bw.flush();
+		bw.close();
+	}
+
+}
